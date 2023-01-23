88f6724323e63d174275c90e27e9233cfa8c0107
Ian Adam Naval <790279+ianonavy@users.noreply.github.com>
2023-01-22 23:48:07 -0500

    Add http server

--- main.go
+++ main.go
@@ -2,32 +2,19 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"github.com/freeeve/uci"
+	"net/http"
 )
 
 func main() {
-	eng, err := uci.NewEngine("/opt/homebrew/bin/stockfish")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// set some engine options
-	eng.SetOptions(uci.Options{
-		Hash:    128,
-		Ponder:  false,
-		OwnBook: true,
-		MultiPV: 4,
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
 	})
 
-	// set the starting position
-	eng.SetFEN("rnb4r/ppp1k1pp/3bp3/1N3p2/1P2n3/P3BN2/2P1PPPP/R3KB1R b KQ - 4 11")
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/chess/", func(w http.ResponseWriter, r *http.Request) {
+		doUci(w)
+	})
 
-	// set some result filter options
-	resultOpts := uci.HighestDepthOnly | uci.IncludeUpperbounds | uci.IncludeLowerbounds
-	results, _ := eng.GoDepth(10, resultOpts)
-
-	// print it (String() goes to pretty JSON for now)
-	fmt.Println(results)
+	http.ListenAndServe(":8080", nil)
 }
